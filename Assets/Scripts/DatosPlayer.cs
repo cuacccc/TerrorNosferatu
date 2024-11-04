d414e18295a60169a072c31c1a14fb45c48b7d98
cuacccc <generalacme@yahoo.com.ar>
2024-11-04 15:01:34 -0300

    se agrego barra de energia al personaje, se le resta al atravesar por el "arboldanino"

--- Assets/Scripts/DatosPlayer.cs
+++ Assets/Scripts/DatosPlayer.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DatosPlayer : MonoBehaviour
 {
 
     public int vidaPlayer;
-
+    public Slider vidaVisual;  
 
     // Update is called once per frame
-    void Update()
+        private void Update()
+
     {
+        vidaVisual.GetComponent<Slider>().value = vidaPlayer;
+
         if (vidaPlayer <= 0)
         {
             Debug.Log("Has muerto");
